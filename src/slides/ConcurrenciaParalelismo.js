2ba50081ee5839482c230d18beadfebb89ca3119
Santiago Ramirez <santiago.ramirez@rootstrap.com>
2023-10-09 17:01:13 -0300

    concurrency and paralelism

--- src/slides/ConcurrenciaParalelismo.js
+++ src/slides/ConcurrenciaParalelismo.js
@@ -1,28 +1,30 @@
 import React from 'react';
+import ConcurrenciaParalelismoImage from '../images/concurrencia_vs_paralelismo.png';
 
 function ConcurrenciaParalelismo() {
   return (
     <section>
-      <section>Concurrencia y paralelismo no son lo mismo</section>
       <section>
-        <h5>Concurrencia</h5>
-        <ul>
-          <li>
-            Eejcucion de multiples tareas que se ejcutan en
-            pero que no necesariamente son al mismo tiempo.
-          </li>
-          <li>Alguna imagen demostrativa.</li>
-        </ul>
+        Concurrencia != paralelismo
+
       </section>
       <section>
-        <h5>Paralelismo</h5>
-        <ul>
-          <li>
-            El paralelismo se refiere a la ejecución real de múltiples tareas
-            al mismo tiempo.
-          </li>
-          <li>Alguna imagen demostrativa.</li>
-        </ul>
+        <img src={ConcurrenciaParalelismoImage} alt="Meme" loading="lazy" />
+
+        <aside className="notes">
+          <p>
+            Primera imagen, imaginense a un desarrollador que tiene 2 tareas asignadas ....
+          </p>
+
+          <p>
+            Segunda imagen, si fueran 2 desaroolladores,
+            la tarea 1 y tarea 2 se pueden ejecutar en paralelo
+          </p>
+
+          <p>
+            Mostrar ejemplo 01.rb
+          </p>
+        </aside>
       </section>
     </section>
   );
